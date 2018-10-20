dbf5ca3857dfcdbca86350118c875bece5f45be8
Orestis Kompougias <elaforestis@gmail.com>
2018-10-20 20:39:47 +0200

    final

--- src/test/Main.java
+++ src/test/Main.java
@@ -2,9 +2,12 @@ package test;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Vector;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -13,22 +16,41 @@ import java.util.concurrent.Future;
 
 public class Main{
 	
-	public static final int EXECUTIONS=10;
+	public static final int EXECUTIONS=1;
+	public static Vector v_cig;
+	public static Vector v_schaffers;
+	public static Vector v_katsuura;
 	
 	public static void main(String args[]) throws InterruptedException, ExecutionException {
+		v_cig=new Vector();
+		v_schaffers=new Vector();
+		v_katsuura=new Vector();
 		
         double results_cigar[]= new double[EXECUTIONS];
         double results_schaffers[]= new double[EXECUTIONS];
-        double results_katsuura[]= new double[(EXECUTIONS/10)-1];
+        //double results_katsuura[]= new double[(EXECUTIONS/10)-1];
+        double results_katsuura[]= new double[EXECUTIONS];
+        
+        
+        long time_cigar=0;
+        long time_schaffers=0;
+        long time_katsuura=0;
         
 		ExecutorService executor = Executors.newFixedThreadPool(50);
 		for(int i =0;i<EXECUTIONS;i++) {
-			//System.out.println(""+i);
+			System.out.println(""+i);
+			long time=System.currentTimeMillis();
 			 results_cigar[i] = (double) executor.submit(new Method("BentCigarFunction")).get();
-			 //results_schaffers[i] = (double) executor.submit(new Method("SchaffersEvaluation")).get();
-			 if(i%10==0 && i!=0) {
-				 //results_katsuura[(i/10)-1] = (double) executor.submit(new Method("KatsuuraEvaluation")).get();
-			 }
+			 time_cigar+=System.currentTimeMillis()-time;
+			 time=System.currentTimeMillis();
+			 results_schaffers[i] = (double) executor.submit(new Method("SchaffersEvaluation")).get();
+			 time_schaffers+=System.currentTimeMillis()-time;
+			 time=System.currentTimeMillis();
+			 results_katsuura[i] = (double) executor.submit(new Method("KatsuuraEvaluation")).get();
+			 time_katsuura+=System.currentTimeMillis()-time;
+			// if(i%10==0 && i!=0) {
+				// results_katsuura[(i/10)-1] = (double) executor.submit(new Method("KatsuuraEvaluation")).get();
+			// }
 		}
 		double sum_cigar=0;
 		double sum_schaffers=0;
@@ -45,7 +67,8 @@ public class Main{
 		for(int i=0;i<EXECUTIONS;i++) {
 			sum_cigar+=results_cigar[i];
 			sum_schaffers+=results_schaffers[i];
-			if(i%10==0 && i!=0) {
+			sum_katsuura+=results_katsuura[i];
+/*			if(i%10==0 && i!=0) {
 				sum_katsuura+=results_katsuura[(i/10)-1];
 				
 				if(results_katsuura[(i/10)-1]>max_katsuura) {
@@ -54,8 +77,13 @@ public class Main{
 				if(results_katsuura[(i/10)-1]<min_katsuura) {
 					min_katsuura=results_katsuura[(i/10)-1];
 				}
+			}*/
+			if(results_katsuura[i]>max_katsuura) {
+				max_katsuura=results_katsuura[i];
 			}
-			
+			if(results_katsuura[i]<min_katsuura) {
+				min_katsuura=results_katsuura[i];
+			}
 			if(results_cigar[i]>max_cigar) {
 				max_cigar=results_cigar[i];
 			}
@@ -71,21 +99,54 @@ public class Main{
 			}
 
 		}
+
 		System.out.println(Arrays.toString(results_cigar));
 		System.out.println("Average ("+EXECUTIONS+" runs) Cigar: "+sum_cigar/EXECUTIONS);
+		System.out.println("Average time ("+EXECUTIONS+" runs) Cigar: "+time_cigar/EXECUTIONS);
 		System.out.println("Max ("+EXECUTIONS+" runs) Cigar: "+max_cigar);
 		System.out.println("Min ("+EXECUTIONS+" runs) Cigar: "+min_cigar);
 		
 		System.out.println(Arrays.toString(results_schaffers));
 		System.out.println("Average ("+EXECUTIONS+" runs) Schaffers: "+sum_schaffers/EXECUTIONS);
+		System.out.println("Average time ("+EXECUTIONS+" runs) Schaffers: "+time_schaffers/EXECUTIONS);
 		System.out.println("Max ("+EXECUTIONS+" runs) Schaffers: "+max_schaffers);
 		System.out.println("Min ("+EXECUTIONS+" runs) Schaffers: "+min_schaffers);
 		
 		System.out.println(Arrays.toString(results_katsuura));
-		System.out.println("Average ("+((EXECUTIONS/10)-1)+" runs) Katsuura: "+sum_katsuura/((EXECUTIONS/10)-1));
+/*		System.out.println("Average ("+((EXECUTIONS/10)-1)+" runs) Katsuura: "+sum_katsuura/((EXECUTIONS/10)-1));
 		System.out.println("Max ("+((EXECUTIONS/10)-1)+" runs) Katsuura: "+max_katsuura);
-		System.out.println("Min ("+((EXECUTIONS/10)-1)+" runs) Katsuura: "+min_katsuura);
+		System.out.println("Min ("+((EXECUTIONS/10)-1)+" runs) Katsuura: "+min_katsuura);*/
+		System.out.println("Average ("+EXECUTIONS+" runs) Katsuura: "+sum_katsuura/EXECUTIONS);
+		System.out.println("Average time ("+EXECUTIONS+" runs) Katsuura: "+time_katsuura/EXECUTIONS);
+		System.out.println("Max ("+EXECUTIONS+" runs) Katsuura: "+max_katsuura);
+		System.out.println("Min ("+EXECUTIONS+" runs) Katsuura: "+min_katsuura);
+		
         executor.shutdown();
+        FileWriter fw;
+        PrintWriter pw = null;
+        try {
+			fw = new FileWriter(new File("avgFitnessPerGen.txt"));
+			pw=new PrintWriter(fw);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		pw.println("Average fitness per gen over 50 runs - Bent Cigar");
+		for(int i=0;i<v_cig.size();i++) {
+			v_cig.set(i, (double)v_cig.get(i)/EXECUTIONS);
+			pw.println((double)v_cig.get(i));
+		}
+		pw.println("Average fitness per gen over 50 runs - Schaffers");
+		for(int i=0;i<v_schaffers.size();i++) {
+			v_schaffers.set(i, (double)v_schaffers.get(i)/EXECUTIONS);
+			pw.println((double)v_schaffers.get(i));
+		}
+		pw.println("Average fitness per gen over 50 runs - Katsuura");
+		for(int i=0;i<v_katsuura.size();i++) {
+			v_katsuura.set(i, (double)v_katsuura.get(i)/EXECUTIONS);
+			pw.println((double)v_katsuura.get(i));
+		}
+		pw.close();
 	}
 
 
--- src/test/Method.java
+++ src/test/Method.java
@@ -31,12 +31,32 @@ public class Method implements Callable{
         String line = null; 
         try {
             while ((line = input.readLine()) != null) {
-                output+=line;
+                output+=line+"\n";
             }
         } catch (IOException e) {
             //e.printStackTrace();
         }
         //System.out.println(output.split("java.lang.NullPointerException")[0]);
+        String scorePerGen[]=output.split("java.lang.")[0].split("\n");
+        for(int i=0;i<scorePerGen.length;i++) {
+        	try {
+	        	if(method.equals("BentCigarFunction")) {
+	        		Main.v_cig.set(i, (double)Main.v_cig.get(i)+Double.parseDouble(scorePerGen[i]));
+	        	}else if(method.equals("SchaffersEvaluation")) {
+	        		Main.v_schaffers.set(i, (double)Main.v_schaffers.get(i)+Double.parseDouble(scorePerGen[i]));
+	        	}else {
+	        		Main.v_katsuura.set(i, (double)Main.v_katsuura.get(i)+Double.parseDouble(scorePerGen[i]));
+	        	}
+        	}catch(ArrayIndexOutOfBoundsException e) {
+	        	if(method.equals("BentCigarFunction")) {
+	        		Main.v_cig.add(i, Double.parseDouble(scorePerGen[i]));
+	        	}else if(method.equals("SchaffersEvaluation")) {
+	        		Main.v_schaffers.add(i, Double.parseDouble(scorePerGen[i]));
+	        	}else {
+	        		Main.v_katsuura.add(i, Double.parseDouble(scorePerGen[i]));
+	        	}
+        	}
+        }
         System.out.println(output.split("Score: ")[1].split("Runtime")[0].trim());
         return Double.parseDouble(output.split("Score: ")[1].split("Runtime")[0].trim());
 	}
